a41264a6fc41797ad9e852c6eb070044d619f9cf
Artem <L0br1Artem@gmail.com>
2023-03-25 10:05:14 +0200

    init commit

---
+++ src/main/java/com/example/lab2/HelloApplication.java
@@ -0,0 +1,15 @@
+package com.example.lab2;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
