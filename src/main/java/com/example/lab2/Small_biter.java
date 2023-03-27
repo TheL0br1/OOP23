20064f3a1fadc0840d7804692ca5bbd98d1bd07a
Artem <L0br1Artem@gmail.com>
2023-03-27 13:48:35 +0300

    anal commitv2

--- src/main/java/com/example/lab2/Small_biter.java
+++ src/main/java/com/example/lab2/Small_biter.java
@@ -12,18 +12,18 @@ public class Small_biter {
 
 
     private static int count;
-    private Position position;
+    private Position position = new Position(0,0);
     private double directionR = 0;
     private double speed = 0;
-    private int id;
+    private final int id;
     private Action action;
     private static final int MaxSpeed;
 
-    public final static  String name = "Small_byter";
-    private final Image spriteImage = new Image(getClass().getResource("Small_biter.png").toExternalForm());
+    private final static  String name = "Small_byter";
+   // private final Image spriteImage = new Image(getClass().getResource("main/sprites/Small_biter.png").toExternalForm());
 
     static{
-        count = 1;
+        count = 0;
         MaxSpeed = 100;
         System.out.println("Class statis initialization");
     }
@@ -31,16 +31,68 @@ public class Small_biter {
         position.X = posX;
         position.Y = posY;
         action = Action.NEUTRAL;
-        count++;
         id = count;
+        count++;
 
     }
     public Small_biter() {
         this(0,0);
     }
+    public String getName(){
+        return name;
+    }
+    public Position getPosition(){
+        return position;
+    }
+    public void setPosition(int x, int y){
+        position.X = x;
+        position.Y = y;
+    }
+    public void setPositionX(int y){
+        setPosition(position.X, y);
+    }
+    public void setPositionY(int x){
+        setPosition(x, position.Y);
+    }
+    public void setDirection(double degreeR){
+        directionR = degreeR;
+    }
+    public double getDirection(){
+        return directionR;
+    }
+    public void setAction(Action action){
+        this.action = action;
+    }
+    public Action getAction(Action action) {
+        return action;
+    }
+    public int getId(){
+        return id;
+    }
+    public void move(int t){
+        move(t, directionR);
+    }
+    public void move(int t, double dir){
+
+        position.X = (int)(t * Math.cos(dir));
+        position.Y = (int)(t * Math.sin(dir));
+
+    }
+    public boolean equals(Small_biter a, Small_biter b){
+        return a == b;
+    }
+    public String toString()
+    {
+        return name + ", located at: " + this.position.X + " - X, " + this.position.Y + " - Y";
+    }
     static private class Position {
-        public int X ;
-        public int Y ;
+        public int X = 0 ;
+        public int Y = 0;
+        public Position(int x, int y){
+                X = x;
+                Y = y;
+        }
+
         // standard getters and setters
     }
 
--- src/main/java/com/example/lab2/main.java
+++ src/main/java/com/example/lab2/main.java
@@ -14,17 +14,23 @@ public class main extends Application {
 
     public static final int CANVAS_WIDTH;
     public static final int CANVAS_HEIGHT;
+    public static final String spriteDirectory;
+
     public static Scanner in;
     static{
         CANVAS_WIDTH = 460;
         CANVAS_HEIGHT = 500;
         in =  new Scanner(System.in);
         System.out.println("static metod initializated");
+        spriteDirectory = System.getProperty("user.dir") + "\\src\\main\\sprites";
     }
     @Override
     public void start(Stage stage) throws Exception{
+
         Canvas canvas = new Canvas(400, 300);
-        Image spriteImage = new Image(getClass().getResource("Small_biter.png").toExternalForm());
+
+        Image spriteImage = new Image("file:" +spriteDirectory+ "\\Small_biter.png");
+        Small_biter first = new Small_biter();
         if (spriteImage == null) {
             System.err.println("Failed to load sprite image");
             System.exit(1);
